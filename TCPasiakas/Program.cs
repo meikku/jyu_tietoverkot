3ebb444a7ac008441a660322b5022cbfbe336fb5
meikku <59999917+meikku@users.noreply.github.com>
2021-05-26 15:43:06 +0300

    first commit

---
+++ TCPasiakas/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+
+namespace TCPasiakas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            s.Connect("localhost", 25000);
+            String snd = "GET / HTTP/1.1\r\nHost: localhost\r\n\r\n";
+
+            byte[] buffer = Encoding.ASCII.GetBytes(snd);
+
+            s.Send(buffer);
+
+            String sivu = "";
+            int count = 0;
+
+            do {
+            byte[] rec = new byte[1024];
+
+            count = s.Receive(rec);
+            Console.Write("tavuja vastaanotettu " + count + "\r\n");
+            sivu += Encoding.ASCII.GetString(rec, 0, count); 
+            } while (count > 0);
+
+            Console.Write(sivu);
+
+            Console.ReadKey();
+            s.Close();
+        }
+    }
+}
---
+++ TCPpalvelin/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+
+namespace TCPpalvelin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket Palvelin = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPEndPoint iep = new IPEndPoint(IPAddress.Loopback, 25000);
+            
+            Palvelin.Bind(iep);
+
+            Palvelin.Listen(5);
+            // while(true) {
+            Socket Asiakas = Palvelin.Accept();
+
+            IPEndPoint iap = (IPEndPoint)Asiakas.RemoteEndPoint;
+
+            Console.WriteLine("Yhteys osoitteesta: {0} portista {1}", iap.Address, iap.Port);
+
+            NetworkStream ns = new NetworkStream(Asiakas);
+
+            StreamReader sr = new StreamReader(ns);
+            StreamWriter sw = new StreamWriter(ns);
+
+            String rec = sr.ReadLine();
+            Console.WriteLine(rec);
+
+            sw.WriteLine("Meikun palvelin;" + rec);
+            sw.Flush();
+            
+            Console.ReadKey();
+            Asiakas.Close(); 
+
+            //} //while    
+            Console.ReadKey();
+
+            Palvelin.Close();
+        }
+    }
+}
